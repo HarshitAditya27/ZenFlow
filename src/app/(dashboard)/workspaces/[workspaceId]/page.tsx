68f6debf2829f7393181e299c33b67ee6d871d62
Harshit Aditya <harshitaditya27@gmail.com>
2024-11-09 10:25:35 +0530

    Added standalone layout

--- src/app/(dashboard)/workspaces/[workspaceId]/page.tsx
+++ src/app/(dashboard)/workspaces/[workspaceId]/page.tsx
@@ -1,4 +1,11 @@
-function WrokspaceId() {
+import { getCurrent } from "@/features/auth/actions";
+import { redirect } from "next/navigation";
+
+async function WrokspaceId() {
+  const user = await getCurrent();
+
+  if (!user) redirect("/sign-in");
+
   return <div>WrokspaceId</div>;
 }
 
