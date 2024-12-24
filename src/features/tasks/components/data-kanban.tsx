c98460ff106c67964d0f2e01c2a6c58756d64204
Harshit Aditya <harshitaditya27@gmail.com>
2024-12-25 01:07:20 +0530

    Added kanban data functionality

---
+++ src/features/tasks/components/data-kanban.tsx
@@ -0,0 +1,63 @@
+import {
+  DragDropContext,
+  Droppable,
+  Draggable,
+  DropResult,
+} from "@hello-pangea/dnd";
+
+import { Task, TaskStatus } from "@/features/tasks/types";
+import { useState } from "react";
+import { KanbanColumnHeader } from "./KanbanColumnHeader";
+
+const boards: TaskStatus[] = [
+  TaskStatus.BACKLOG,
+  TaskStatus.TODO,
+  TaskStatus.IN_PROGRESS,
+  TaskStatus.IN_REVIEW,
+  TaskStatus.DONE,
+];
+
+type TaskState = {
+  [key in TaskStatus]: Task[];
+};
+
+interface DataKanbanProps {
+  data: Task[];
+}
+
+export const DataKanban = ({ data }: DataKanbanProps) => {
+  const [tasks, setTasks] = useState<TaskState>(() => {
+    const initialTask: TaskState = {
+      [TaskStatus.BACKLOG]: [],
+      [TaskStatus.TODO]: [],
+      [TaskStatus.IN_PROGRESS]: [],
+      [TaskStatus.IN_REVIEW]: [],
+      [TaskStatus.DONE]: [],
+    };
+    data.forEach((task) => initialTask[task.status].push(task));
+    Object.keys(initialTask).forEach((status) => {
+      initialTask[status as TaskStatus].sort((a, b) => a.position - b.position);
+    });
+    return initialTask;
+  });
+
+  return (
+    <DragDropContext onDragEnd={() => {}}>
+      <div className="flex overflow-x-auto">
+        {boards.map((board) => {
+          return (
+            <div
+              key={board}
+              className="flex-1 mx-2 bg-muted p-1.5 rounded-md min-w-[200px]"
+            >
+              <KanbanColumnHeader
+                board={board}
+                taskCount={tasks[board].length}
+              />
+            </div>
+          );
+        })}
+      </div>
+    </DragDropContext>
+  );
+};
