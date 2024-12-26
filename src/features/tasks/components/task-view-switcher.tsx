7c181edb118abf25fff1a84c2a3160364f7c16cb
Harshit Aditya <harshitaditya27@gmail.com>
2024-12-26 14:23:50 +0530

    Added data calendar functionality and component for projects

--- src/features/tasks/components/task-view-switcher.tsx
+++ src/features/tasks/components/task-view-switcher.tsx
@@ -16,6 +16,7 @@ import { DataKanban } from "./data-kanban";
 import { useCallback } from "react";
 import { TaskStatus } from "../types";
 import { useBulkUpdateTask } from "../api/use-bulk-update-tasks";
+import DataCalendar from "./DataCalendar";
 
 export const TaskViewSwitcher = () => {
   const [{ status, assigneeId, projectId, dueDate }] = useTaskFilters();
@@ -85,8 +86,9 @@ export const TaskViewSwitcher = () => {
                 data={tasks?.documents ?? []}
               />
             </TabsContent>
-            <TabsContent value="calendar" className="mt-0">
-              {JSON.stringify(tasks)}
+            <TabsContent value="calendar" className="mt-0 h-full pb-4">
+              {/* {JSON.stringify(tasks)} */}
+              <DataCalendar data={tasks?.documents ?? []} />
             </TabsContent>
           </>
         )}
