b2fdd9535dcdc48ad53916bdeda588ecb6b4a743
Harshit Aditya <harshitaditya27@gmail.com>
2024-12-27 23:57:17 +0530

    Added project analytics functionality

---
+++ src/components/Analytics.tsx
@@ -0,0 +1,77 @@
+// interface AnalyticsProps {
+//   data?: {
+//     taskCount: number;
+//     taskDifference: number;
+//     projectCount?: number;
+//     projectDifference?: number;
+//     incompleteTaskCount?: number;
+//     incompleteTaskDifference?: number;
+//     assignedTaskCount: number;
+//     assignedTaskDifference: number;
+//     completedTaskCount: number;
+//     completedTaskDifference: number;
+//     overdueTaskCount: number;
+//     overdueTaskDifference: number;
+//   };
+// }
+
+import { ProjectAnalyticsResponseType } from "@/features/projects/api/use-get-project-analytics";
+import { ScrollArea, ScrollBar } from "./ui/scroll-area";
+import { AnalyticsCard } from "./AnalyticsCard";
+import { DottedSeperator } from "./dotted-seperator";
+import { Scroll } from "lucide-react";
+
+export const Analytics = ({ data }: ProjectAnalyticsResponseType) => {
+  return (
+    <ScrollArea className="border rounded-lg w-full whitespace-nowrap shrink-0">
+      <div className="w-full flex flex-row">
+        <div className="flex items-center flex-1">
+          <AnalyticsCard
+            title="Total task"
+            value={data.taskCount}
+            variant={data.taskDifference > 0 ? "up" : "down"}
+            increaseValue={data.taskDifference}
+          />
+          <DottedSeperator direction="vertical" />
+        </div>
+        <div className="flex items-center flex-1">
+          <AnalyticsCard
+            title="Assigned Task"
+            value={data.assignedTaskCount}
+            variant={data.assignedTaskDifference > 0 ? "up" : "down"}
+            increaseValue={data.assignedTaskDifference}
+          />
+          <DottedSeperator direction="vertical" />
+        </div>
+        <div className="flex items-center flex-1">
+          <AnalyticsCard
+            title="Completed Task"
+            value={data.completedTaskCount}
+            variant={data.completedTaskDifference > 0 ? "up" : "down"}
+            increaseValue={data.completedTaskDifference}
+          />
+          <DottedSeperator direction="vertical" />
+        </div>
+        <div className="flex items-center flex-1">
+          <AnalyticsCard
+            title="Overdue Task"
+            value={data.overdueTaskCount}
+            variant={data.overdueTaskDifference > 0 ? "up" : "down"}
+            increaseValue={data.overdueTaskDifference}
+          />
+          <DottedSeperator direction="vertical" />
+        </div>
+        <div className="flex items-center flex-1">
+          <AnalyticsCard
+            title="Incomplete Task"
+            value={data.incompleteTaskCount}
+            variant={data.incompleteTaskDifference > 0 ? "up" : "down"}
+            increaseValue={data.incompleteTaskDifference}
+          />
+          <DottedSeperator direction="vertical" />
+        </div>
+      </div>
+      <ScrollBar orientation="horizontal" />
+    </ScrollArea>
+  );
+};
