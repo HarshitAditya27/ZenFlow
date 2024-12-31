dc254b5739f8c51fb24a29ffcb87dd6c06fbab93
Harshit Aditya <harshitaditya27@gmail.com>
2024-12-31 10:59:28 +0530

    Fixed image info typo and removed unnecessary components

--- src/features/tasks/components/data-table.tsx
+++ src/features/tasks/components/data-table.tsx
@@ -112,7 +112,7 @@ export function DataTable<TData, TValue>({
           </TableBody>
         </Table>
       </div>
-      <div className="flex items-center justify-end space-x-2 py-4">
+      {/* <div className="flex items-center justify-end space-x-2 py-4">
         <Button
           variant="outline"
           size="sm"
@@ -129,7 +129,7 @@ export function DataTable<TData, TValue>({
         >
           Next
         </Button>
-      </div>
+      </div> */}
     </div>
   );
 }
